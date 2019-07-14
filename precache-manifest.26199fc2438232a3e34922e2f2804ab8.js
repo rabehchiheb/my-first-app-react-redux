3ebae89a87109a8bb9ff3c711b6eec6ddd005051
chiheb <chiheb.rabeh@gmail.com>
2019-07-14 10:07:59 +0100

    Updates

---
+++ precache-manifest.26199fc2438232a3e34922e2f2804ab8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f08970be11b06c6184ea0e4874eddd",
+    "url": "/my-first-app-react-redux/./index.html"
+  },
+  {
+    "revision": "9e40892224ebc2461c19",
+    "url": "/my-first-app-react-redux/./static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "e8b462a4f4ccadbfc2d2",
+    "url": "/my-first-app-react-redux/./static/js/2.7a364b00.chunk.js"
+  },
+  {
+    "revision": "9e40892224ebc2461c19",
+    "url": "/my-first-app-react-redux/./static/js/main.16666772.chunk.js"
+  },
+  {
+    "revision": "010b548cd944cb232e36",
+    "url": "/my-first-app-react-redux/./static/js/runtime~main.7b3c8670.js"
+  }
+]);
